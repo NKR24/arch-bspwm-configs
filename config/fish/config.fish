870c817dc935fac8358d5f7dac9a373a3ec8c93b
Nikita Khvorostin <khvorostinn@gmail.com>
2024-09-10 21:42:37 +0300

    new upload config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -32,5 +32,3 @@ set -gx BROWSER /usr/bin/firefox
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
